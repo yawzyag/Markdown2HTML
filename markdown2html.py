5c857cb54e3acc58991d08889aceb2cf2d921347
yawzyag <yesid.dev93@gmail.com>
2020-07-30 14:57:01 -0500

    [Fix] remove print

--- markdown2html.py
+++ markdown2html.py
@@ -32,7 +32,6 @@ def read_file(filename="", outputfile=""):
         for line in juanito:
             if(line.strip()[0] == "#"):
                 finall_text += transform_title(line.strip())
-        print(finall_text)
         write_file(outputfile, finall_text)
 
 
@@ -40,7 +39,8 @@ def main(argv):
     """markdown to html func"""
     args = sys.argv[1:]
     if (len(args) < 2):
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
     if (not os.path.isfile(args[0])):
         print("Missing", args[0], file=sys.stderr)

e99a4629662069e1fd288896a7f0760f65cb9d3f
yawzyag <yesid.dev93@gmail.com>
2020-07-30 14:47:50 -0500

    [Feature] headings

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Markdown2HTML
+# My title
+## My title2
+# My title3
+#### My title4
+### My title5
--- markdown2html.py
+++ markdown2html.py
@@ -4,6 +4,37 @@ import sys
 import os
 
 
+def write_file(filename="", text=""):
+    """write the html file"""
+    with open(filename, mode="w", encoding="UTF8") as juanito:
+        return juanito.write(text)
+
+
+def transform_title(string_title=""):
+    """transform html format title to html"""
+    chars_to_remove = ["#"]
+    hsize = 0
+    for letter in string_title:
+        if (letter == "#"):
+            hsize += 1
+    html_title = "".join(
+        i for i in string_title if not i in chars_to_remove).strip()
+    heading_format_start = "<h{}>".format(hsize)
+    heading_format_end = "</h{}>".format(hsize)
+    return "{}{}{}\n".format(heading_format_start, html_title, heading_format_end)
+
+
+def read_file(filename="", outputfile=""):
+    """read the md file"""
+    with open(filename, encoding="UTF8") as juanito:
+        finall_text = ""
+        for line in juanito:
+            if(line.strip()[0] == "#"):
+                finall_text += transform_title(line.strip())
+        print(finall_text)
+        write_file(outputfile, finall_text)
+
+
 def main(argv):
     """markdown to html func"""
     args = sys.argv[1:]
@@ -14,6 +45,9 @@ def main(argv):
     if (not os.path.isfile(args[0])):
         print("Missing", args[0], file=sys.stderr)
         sys.exit(1)
+    inputFile = args[0]
+    outputfile = args[1]
+    read_file(inputFile, outputfile)
     exit(0)
 
 

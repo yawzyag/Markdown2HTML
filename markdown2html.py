256019ac1774b4823bdcd3acae08fb49c3c01c12
yawzyag <yesid.dev93@gmail.com>
2020-07-30 15:09:23 -0500

    [Feature] headings

--- markdown2html.py
+++ markdown2html.py
@@ -41,10 +41,11 @@ def main(argv):
     """markdown to html func"""
     args = sys.argv[1:]
     if (len(args) < 2):
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
     if (not os.path.isfile(args[0])):
-        print("Missing", args[0])
+        print("Missing", args[0], file=sys.stderr)
         sys.exit(1)
     inputFile = args[0]
     outputfile = args[1]

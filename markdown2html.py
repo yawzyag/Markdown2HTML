bc1b7e66030b0a4146142277818d3e9e1e9194b4
yawzyag <yesid.dev93@gmail.com>
2020-07-30 14:07:17 -0500

    [Feature] argv

--- markdown2html.py
+++ markdown2html.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
 """markdown to html"""
 import sys
-import getopt
 import os
 
 
 def main(argv):
     """markdown to func"""
-    inputfile = ''
-    outputfile = ''
-    try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-        if (len(args) < 2):
-            # print to standart error
-            print("Usage: ./markdown2html.py README.md README.html",
-                  file=sys.stderr)
-            sys.exit(1)
-        if (not os.path.isfile(args[0])):
-            print("Missing", args[0], file=sys.stderr)
-            sys.exit(1)
-    except Exception as ex:
-        print(ex)
+    args = sys.argv[1:]
+    if (len(args) < 2):
+        # print to standart error
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
+    if (not os.path.isfile(args[0])):
+        print("Missing", args[0], file=sys.stderr)
+        sys.exit(1)
     exit(0)
 
 
 if __name__ == "__main__":
     """pass to main func"""
-    main(sys.argv[1:])
+    main(sys.argv)

09649cf3fec772983bea3cc08a257afbc57994a2
yawzyag <yesid.dev93@gmail.com>
2020-07-31 12:41:10 -0500

    [Feature] list

--- markdown2html.py
+++ markdown2html.py
@@ -12,8 +12,8 @@ def write_file(filename="", text=""):
 
 def transform_title(string_title=""):
     """transform html format title to html"""
-    chars_to_remove = ["#"]
     hsize = 0
+    chars_to_remove = ["#"]
     for letter in string_title:
         if (letter == "#"):
             hsize += 1
@@ -25,15 +25,32 @@ def transform_title(string_title=""):
                              html_title, heading_format_end)
 
 
+def transform_list(string_list=""):
+    """transform html format list to html"""
+    chars_to_remove = ["-"]
+    string_list = "".join(
+        i for i in string_list if i not in chars_to_remove).strip()
+    return "<li>{}</li>\n".format(string_list)
+
+
 def read_file(filename="", outputfile=""):
     """read the md file"""
     with open(filename, encoding="UTF8") as juanito:
         finall_text = ""
         for line in juanito:
-            if(line.strip()[0] == "#"):
+            text_list = ""
+            if(len(line) > 1 and line.strip()[0] == "#"):
                 finall_text += transform_title(line.strip())
+            elif (len(line) > 1 and line.strip()[0] == "-"):
+                if (finall_text[-6:-1] == "</ul>"):
+                    finall_text = finall_text[0:-6]
+                else:
+                    text_list += "<ul>\n"
+                text_list += transform_list(line.strip())
+                text_list += "</ul>\n"
             else:
-                finall_text += line
+                finall_text += ""
+            finall_text += text_list
         write_file(outputfile, finall_text)
 
 
